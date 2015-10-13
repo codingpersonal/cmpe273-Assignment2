576a7f0e000613c587e391a25f8f848839b9b18f
Saurabh Gupta <saurabg@fb.com>
2015-10-12 23:37:51 -0700

    create location done

--- client/Client.go
+++ client/Client.go
@@ -11,13 +11,17 @@ import (
 func main() {
 
 	client := &http.Client{}
-	var req1 LocationService;
-	req1.Name = "Saurabh1"
-	req1.Address = "1055 E Evelyn Ave Sunnyvale CA"
-	reqBody, err := json.Marshal(req1)
+	var req LocationService;
+	req.Name = "Saurabh1"
+	req.Address = "1055 E Evelyn Ave"
+	req.City = "Sunnyvale"
+	req.State = "CA"
+	req.Zip = "94086"
 	
-	req, err := http.NewRequest("PUT", "http://localhost:8080/locations/1243", strings.NewReader(string(reqBody)))
-	resp, err := client.Do(req)
+	reqBody, err := json.Marshal(req)
+	
+	req1, err := http.NewRequest("POST", "http://localhost:8080/locations", strings.NewReader(string(reqBody)))
+	resp, err := client.Do(req1)
 
 	fmt.Println(resp,err);
 	if err != nil {
--- server/servicemethods.go
+++ server/servicemethods.go
@@ -5,19 +5,51 @@ import (
 		"fmt"
 		"io/ioutil"
 		"encoding/json"
-		"github.com/gorilla/mux"
+//		"github.com/gorilla/mux"
+		"math/rand"
+		"strconv"
 )
 
 // defines all the REST APIs handlers
 
+// this will vlaidate the response from google with the req asked by user
+func ValidateResponseWithRequest(res LocationService, req LocationService) bool {
+	return res.City == req.City && res.Zip == req.Zip
+}
+
 // kind of stubs
 func CreateLocation(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("inside create location fn");
+	body, err := ioutil.ReadAll(r.Body)
+	
+	var req LocationService
+	err = json.Unmarshal(body, &req)
+	
+	googleresp := getGoogleLocation(req.Address + "+" + req.City + "+" + req.State + "+" + req.Zip);
+    fmt.Println("resp is: ", googleresp);
+	
+	if !ValidateResponseWithRequest(googleresp, req) {
+		// modify the request object itself
+		req.ErrorMsg = "Invalid Address. No such address exists as per Google service";
+	} else {
+	    req.Id = strconv.Itoa(rand.Intn(10000))
+	    req.Coordinate.Lat = googleresp.Coordinate.Lat
+	    req.Coordinate.Lng = googleresp.Coordinate.Lng
+
+	    // TODO: store the response in the mongo db
+    }
+    
+    json.NewEncoder(w).Encode(req)
+    
+    if err != nil {
+    	fmt.Println("some error");
+    }  
 
 }
 
 func GetLocation(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("inside Get location fn");
+//	    vars := mux.Vars(r)
+	
 
 }
 
@@ -27,24 +59,4 @@ func DeleteLocation(w http.ResponseWriter, r *http.Request) {
 
 
 func PutLocation(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    Id := vars["id"]
-	body, err := ioutil.ReadAll(r.Body)
-	
-	var req LocationService
-	err = json.Unmarshal(body, &req)
-	
-	googleresp := getGoogleLocation(req.Address);
-	fmt.Println(googleresp);
-	
-    // get data for that id from mongo
-    var res LocationService;
-    res.Id = Id
-    res.Name = req.Name
-    
-    json.NewEncoder(w).Encode(res)
-    
-    if err != nil {
-    	fmt.Println("some error");
-    }  
 }
