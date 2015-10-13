2533b980b34eda51af922407975c2834a611adf7
codingpersonal <codingpersonal@gmail.com>
2015-10-13 00:19:46 -0700

    Update servicemethods.go

--- server/servicemethods.go
+++ server/servicemethods.go
@@ -46,17 +46,66 @@ func CreateLocation(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func GetLocation(w http.ResponseWriter, r *http.Request) {
+func GetLocation(w http.ResponseWriter, r *http.Request) LocationService{
 	fmt.Println("inside Get location fn");
-//	    vars := mux.Vars(r)
+	vars := mux.Vars(r)
+	location_id := vars["id"]
+	var res LocationService
 	
-
+	//Get the response from Mongo Db for this Location_Id
+	
+	//If error is nil, then fill the response structure
+	res.Id = "Dummy" 
+	res.Name = "Dummy"
+	res.Address = "Dummy"
+	res.City = "Dummy"
+	res.State = "Dummy"
+	res.Zip = "Dummy"
+	res.Coordinate.Lat = "Dummy"
+	res.Coordinate.Lng = "Dummy"
+	res.ErrorMsg = nil
+    
+    json.NewEncoder(w).Encode(res)
+    return res
 }
 
 func DeleteLocation(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("inside Delete location fn");
+	vars := mux.Vars(r)
+	location_id := vars["id"]
+	
+	//Delete this location id data from Mongo Db
 }
 
 
-func PutLocation(w http.ResponseWriter, r *http.Request) {
+func PutLocation(w http.ResponseWriter, r *http.Request) LocationService {
+	vars := mux.Vars(r)
+	location_id := vars["id"]
+	body, err := ioutil.ReadAll(r.Body)
+	
+	var req LocationService
+	var res LocationService
+	err = json.Unmarshal(body, &req)
+	
+	googleresp := getGoogleLocation(req.Address + "+" + req.City + "+" + req.State + "+" + req.Zip);
+    fmt.Println("resp is: ", googleresp);
+	
+	if !ValidateResponseWithRequest(googleresp, req) {
+		// modify the request object itself
+		res.ErrorMsg = "Invalid Address, cannot update. No such address exists as per Google service";
+	} else {
+	    res.location_id = location_id
+	    res.Address = req.Address
+	    res.City = req.City
+	    res.State = req.State
+	    res.Zip = req.Zip
+	    res.ErrorMsg = nil
+	    res.Coordinate.Lat = req.Coordinate.Lat
+	    res.Coordinate.Lng = req.Coordinate.Lng
+
+	    // TODO: store the response in the mongo db for this Location ID 
+    }
+    
+    json.NewEncoder(w).Encode(res)
+    return res
 }
