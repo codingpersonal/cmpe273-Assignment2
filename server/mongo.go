02ad72ca621472b64c9165038eaa2e428e2163df
Saurabh Gupta <saurabg@fb.com>
2015-10-18 12:05:42 -0700

    mongo add in family

--- server/mongo.go
+++ server/mongo.go
@@ -1,19 +1,20 @@
 package main
 
-//import ("gopkg.in/mgo.v2"
-//		"gopkg.in/mgo.v2/bson"
-//)
-//
-//func getSession() *mgo.Session {  
-//    // Connect to our local mongo
-//    s, err := mgo.Dial("mongodb://localhost:8081")
-//
-//    // Check if connection error, is mongo running?
-//    if err != nil {
-//        panic(err)
-//    }
-//    return s
-//}
+import (
+		"gopkg.in/mgo.v2"
+		"gopkg.in/mgo.v2/bson"
+)
+
+func getSession() *mgo.Session {  
+    // Connect to our local mongo
+    s, err := mgo.Dial("mongodb://localhost:8081")
+
+    // Check if connection error, is mongo running?
+    if err != nil {
+        panic(err)
+    }
+    return s
+}
 
 func getData(location_id string) LocationService{
 	var a LocationService
